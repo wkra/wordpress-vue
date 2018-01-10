190765660df6052efcf1e591ec79cf4441b3ac72
wkra <wkrawiec@gmail.com>
2018-01-10 23:50:54 +0100

    data from api works

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,43 @@
 <template>
   <div class="home">
     <h1>{{ msg }}</h1>
+    <div class="post__wrapper" v-for="post in posts">
+      <h2>{{ post.title.rendered }}</h2>
+      <span v-html="post.content.rendered"></span>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'HOME',
+      posts: ''
     }
+  },
+    // Fetches posts when the component is created.
+  created () {
+    axios.get(`http://localhost/wordpress-vue/wp-json/wp/v2/posts/`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.posts = response.data
+      console.log(response.data)
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
   }
 }
 </script>
